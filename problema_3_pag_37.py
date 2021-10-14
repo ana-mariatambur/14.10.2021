393d02a724921195b3fdfc3d226fc09e180f8463
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2021-10-14 11:14:25 +0300

    Add files via upload

--- problema_3_pag_37.py
+++ problema_3_pag_37.py
@@ -1,4 +1,4 @@
-n=(str(input("ce nume si prenume ai?")))
-m=(str(input("ce iti doresti sa devii in viitor?")))
-print("numele tau",n)
-print("profesia dorita",m)
+v=(str(input("ce nume si prenume ai?\n")))
+w=(str(input("ce iti doresti sa devii in viitor?\n")))
+print("numele tau",v)
+print("profesia dorita",w)
